02d9961e9ceba34a7d6f42d7092fcf7b14c77aa6
Jiyun Park <qkrwldbs1216@gmail.com>
2021-11-11 23:10:51 -0500

    updated

---
+++ learning.py
@@ -0,0 +1,64 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+
+training_data = datasets.MNIST(
+    root="data",
+    train = True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.MNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+labels_map = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+
+}
+
+def runc():
+    figure = plt.figure(figsize=(8,8))
+    cols, rows = 3,3
+    for i in range(1, cols*rows + 1):
+        sample_idx = torch.randint(len(training_data), size=(1,)).item()
+        img, label = training_data[sample_idx]
+        figure.add_subplot(rows,cols, i)
+        plt.axis("off")
+        plt.imshow(img.squeeze(), cmap="gray")
+    plt.show()
+
+def tesr():
+    # Display image and label.
+    train_features, train_labels = next(iter(train_dataloader))
+    print(f"Feature batch shape: {train_features.size()}")
+    print(f"Labels batch shape: {train_labels.size()}")
+    img = train_features[0].squeeze()
+    label = train_labels[0]
+    plt.imshow(img, cmap="gray")
+    print(f"Label: {label}")
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    tesr()
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
-DATA_DIR = "data/"
-TEST_DATA_FILENAME = DATA_DIR + 't10k-images.idx3-ubyte'
-TEST_LABELS_FILENAME = DATA_DIR + 't10k-labels.idx1-ubyte'
-TRAIN_DATA_FILENAME = DATA_DIR + 'train-images.idx3-ubyte'
-TRAIN_LABELS_FILENAME = DATA_DIR + 'train-labels.idx1-ubyte'
+DATA_DIR = "data/MNIST/raw/"
+TEST_DATA_FILENAME = DATA_DIR + 't10k-images-idx3-ubyte'
+TEST_LABELS_FILENAME = DATA_DIR + 't10k-labels-idx1-ubyte'
+TRAIN_DATA_FILENAME = DATA_DIR + 'train-images-idx3-ubyte'
+TRAIN_LABELS_FILENAME = DATA_DIR + 'train-labels-idx1-ubyte'
 
 def bytes_to_int(byte_data):
     return int.from_bytes(byte_data, 'big')
@@ -46,24 +46,47 @@ def flatten_list(l):
 
 def extract_features(X):
     return [flatten_list(sample) for sample in X]
-    return flatten_list(X)
 
-def knn(X_train, Y_train, X_test, k=3):
+def dist(x,y):
+    return sum(
+        [
+            (bytes_to_int(x_i)-bytes_to_int(y_i)) ** 2 for x_i, y_i in zip(x,y)]
+               ) ** (0.5)
+
+def get_training_distances_for_test_sample(X_train, test_sample):
+    return [dist(train_sample, test_sample) for train_sample in X_train]
+
+
+
+def knn(X_train, Y_train, X_test, y_test, k=3):
+    y_pred = []
+    for test_sample_idx, test_sample in enumerate(X_test):
+        training_distance = get_training_distances_for_test_sample(X_train, test_sample)
+
+        sorted_distance_indices = [
+            pair[0] for pair in sorted(enumerate(training_distance), key=lambda x: x[1])
+        ]
+        candidates = [
+            bytes_to_int(Y_train[idx]) for idx in sorted_distance_indices[:k]
+        ]
+        print(f'Point is {bytes_to_int(y_test[test_sample_idx])} and we guessed {candidates}')
+        y_sample = 5
+        y_pred.append(y_sample)
+    return y_pred
 
 
 def main():
-    X_train = read_images(TRAIN_DATA_FILENAME, 100)
-    X_test = read_images(TEST_DATA_FILENAME,100)
-
-    print(len(X_train[0]))
-    print(len(X_test[0]))
-
+    X_train = read_images(TRAIN_DATA_FILENAME,3000)
+    y_train = read_labels(TRAIN_LABELS_FILENAME)
+    X_test = read_images(TEST_DATA_FILENAME,10)
+    y_test = read_labels(TEST_LABELS_FILENAME)
 
     X_train = extract_features(X_train)
     X_test = extract_features(X_test)
 
-    print(len(X_train[0]))
-    print(len(X_test[0]))
+    knn(X_train, y_train, X_test, y_test, 3)
+
+
 
 if __name__ == '__main__':
     main()
